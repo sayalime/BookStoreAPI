e3ff5fbc199d39151b1620600698bd56f5e9f083
sayal <sayalipawar2468@gmail.com>
2026-07-28 12:02:57 +0530

    model Valildation

--- BookStoreAPI/Models/Book.cs
+++ BookStoreAPI/Models/Book.cs
@@ -1,10 +1,18 @@
-﻿namespace BookStoreAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreAPI.Models
 {
     public class Book
     {
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Author {  get; set; }
+
+        [Range(1, 1000000)]
         public decimal Price { get; set; }
 
     }
