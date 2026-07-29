04b99dcd11216544367537d1f86f80f0a7b38968
sayal <sayalipawar2468@gmail.com>
2026-07-29 13:42:12 +0530

    repository added and structure folders  created

---
+++ BookStoreAPI/Interfaces/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using BookStoreAPI.Models;
+
+namespace BookStoreAPI.Interfaces
+{
+    public interface IBookRepository
+    {
+        //Retrieve all books asynchronously and return them as a collection.
+        public Task<IEnumerable<Book>> GetAllAsync();
+
+        public Task<Book?> GetByIdAsync(int id);
+
+        public Task AddAsync(Book book);
+
+        public Task UpdateAsync(Book book);
+        public Task DeleteAsync(Book book);
+    }
+}
