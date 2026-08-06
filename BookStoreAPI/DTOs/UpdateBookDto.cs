34415c59409224a6f617b5734aa4dbd4f275c6f0
sayal <sayalipawar2468@gmail.com>
2026-08-06 13:37:08 +0530

    model Validation

--- BookStoreAPI/DTOs/UpdateBookDto.cs
+++ BookStoreAPI/DTOs/UpdateBookDto.cs
@@ -5,10 +5,12 @@ namespace BookStoreAPI.DTOs
     public class UpdateBookDto
     {
         [Required]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Title must be between 2 and 100 characters.")]
         public string Title { get; set; } = string.Empty;
         [Required]
+        [StringLength(100, ErrorMessage = "Author must be between 2 and 100 characters.")]
         public string Author { get; set; } = string.Empty;
-        [Range(1, 100000) ]
+        [Range(1, 100000, ErrorMessage = "Price must be between 1 and 100000.")]
         public decimal Price { get; set; }
     }
 }
